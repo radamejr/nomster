cf72cfd710d7db2b5649a1779b735fc842239b98
radamejr <radamejr@gmail.com>
2019-06-11 18:08:33 +0000

    Added actualy submission to user form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,5 +12,14 @@ class PlacesController < ApplicationController
   end
 
 
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 
 end
