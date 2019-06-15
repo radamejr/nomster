fdc77bc20c36baec80ca5f2e5434fa58f9008333
radamejr <radamejr@gmail.com>
2019-06-15 18:49:01 +0000

    Added edit functionality

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -25,6 +25,12 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
   private
   
   def place_params
