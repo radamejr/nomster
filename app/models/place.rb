e26d20273c438bfa87fad26693cfff10388cc0a6
radamejr <radamejr@gmail.com>
2019-06-18 19:45:49 +0000

    Added DB for user comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
+
   validates :name, presence: true, length: {minimum: 3}
   validates :address, presence: true
   validates :description, presence: true
