9b7707bcb87c9d5eaa1fba55546c28d590cf9220
radamejr <radamejr@gmail.com>
2019-06-21 00:04:46 +0000

    Added username to Users so that username is displayed instead of email.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
-
+  before_action :configure_permitted_parameters, if: :devise_controller?  
   def index
   end
+
+  protected
+    def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
+     devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :password_confirmation])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
+    end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :login
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
 
   has_many :places
   has_many :comments
+
+  def self.find_for_database_authentication warden_conditions
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
+  end
+
 end
