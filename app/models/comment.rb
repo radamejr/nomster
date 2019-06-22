ac070100205dafbaaa1dfea2f45bca2f5485bb73
radamejr <radamejr@gmail.com>
2019-06-22 19:35:11 +0000

    Added pop up modal for submitting comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place 
+
+  RATINGS = {
+    'One Star': '1_star',
+    'Two Stars': '2_stars',
+    'Three Stars': '3_stars',
+    'Four Stars': '4_stars',
+    'Five Stars': '5_stars'
+  }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
